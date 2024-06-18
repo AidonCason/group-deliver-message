caf723fe53d5137fdbcfed2a667ca7d53e928252
aidon <silverblade999@outlook.com>
2024-06-19 00:46:58 +0800

    仅发送好友私聊

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,32 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, Logger, h } from 'koishi';
+import {} from 'koishi-plugin-adapter-onebot';
 
-export const name = 'group-deliver-message'
+export const name = 'group-deliver-message';
 
 export interface Config {}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({});
+
+const logger = new Logger('group-deliver-message');
 
 export function apply(ctx: Context) {
   // write your plugin here
+  ctx.command('群发 <group>').action(async (argv, group) => {
+    logger.info('argv:%o', argv);
+    logger.info('group:%o', group);
+    const guildId = group ?? argv.session.guildId;
+    if (guildId) {
+      logger.info('guildId:%o', guildId);
+      const members = argv.session.bot.getGuildMemberIter(guildId);
+      for await (const member of members) {
+        if (member.user.id == '108720516' || member.user.id == '3563720030') {
+          logger.info('member:%o', member);
+          if (argv.session.onebot) {
+            logger.info('member:%o 111', member);
+            argv.session.onebot.sendPrivateMsg(member.user.id, '你好');
+          }
+        }
+      }
+    }
+  });
 }
